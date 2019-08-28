25cabb5eb5884fd182a875ab91437d8b9f530d7c
PauMB <paumb85@gmail.com>
2019-08-28 17:14:35 +0200

    se almacenan citas en el localStorage

--- src/store.js
+++ src/store.js
@@ -1,14 +1,21 @@
 import { createStore } from 'redux';
 import reducer from './reducers/index';
+import { obtenerStateStorage, guardarStateStorage } from './localstorage';
 
-//State inicial
-//const initialState = [];
+//State inicial, obtenemos las citas del storage(si hay...)
+const initialState = obtenerStateStorage();
 
 
 const store = createStore(
     reducer,
-    //initialState,
+    initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe( () => {
+    guardarStateStorage({
+        citas: store.getState().citas
+    })
+})
 
 export default store;
